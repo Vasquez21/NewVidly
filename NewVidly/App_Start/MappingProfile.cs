861de058c04022e97124403a5511711f4cc2cf90
Michalowski, Michal <m.michalowski@dieboldnixdorf.com>
2020-04-03 19:45:20 +0100

    movies dto

--- NewVidly/App_Start/MappingProfile.cs
+++ NewVidly/App_Start/MappingProfile.cs
@@ -14,6 +14,8 @@ namespace NewVidly.App_Start
         {
             Mapper.CreateMap<Customer, CustomerDto>();
             Mapper.CreateMap<CustomerDto, Customer>();
+            Mapper.CreateMap<Movie, MovieDto>();
+            Mapper.CreateMap<MovieDto, Movie>();
         }
     }
 }
