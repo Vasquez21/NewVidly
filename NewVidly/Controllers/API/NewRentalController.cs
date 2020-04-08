ffbcad8296dced44082335420b23e4ccca20f0f3
Michalowski, Michal <m.michalowski@dieboldnixdorf.com>
2020-04-08 14:11:47 +0100

    Adding Rental

---
+++ NewVidly/Controllers/API/NewRentalController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using NewVidly.Dtos;
+
+namespace NewVidly.Controllers.API
+{
+    public class NewRentalController : ApiController
+    {
+        // POST /api/movies 
+        [HttpPost] 
+        public IHttpActionResult CreateNewRental(RentalDto rentalDto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ NewVidly/Dtos/RentalDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NewVidly.Dtos
+{
+    public class RentalDto
+    {
+        public byte Id { get; set; }
+        public string Name { get; set; }
+    }
+}
